fcb195ac1b6981236b6ddb9ff819ed509e225c55
JerryYangHub <rui_yang1993@163.com>
2017-03-31 21:06:00 +0800

    add shell merge quick select sort

--- Sort.cpp
+++ Sort.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
 //排序类
 class CSort
 {
@@ -11,6 +7,20 @@ public:
 	static void BubbleSort(vector<int> &a);
 	//插入排序法
 	static void InsertSort(vector<int> &a);
+	//希尔排序法
+	static void ShellSort(vector<int> &a);
+	//选择排序法
+	static void SelectSort(vector<int> &a);
+	//归并排序
+	static void MergeSort(vector<int> &a);
+	//快速排序
+	static void QuickSort(vector<int> &a);
+private:
+	//合并有序数组
+	static void MergeArray(vector<int> &a,int left,int middle,int right);
+	static void mergesort(vector<int> &a,int left,int right);
+	static void qiucksort(vector<int> &a,int left,int right);
+
 
 
 };
@@ -59,16 +69,100 @@ void CSort::InsertSort(vector<int> &a)
 	}
 }
 
-int main()
+//希尔排序 等间隔排序 再缩短间隔
+void CSort::ShellSort(vector<int> &a)
 {
-	cout<<"Sort test.........."<<endl;
-	cout<<"array: ";
-	int a[] ={3,3,4,5,81,33,97,10,0};
-	vector<int> _array(a,a+9);
-	CSort::print(_array);
-	CSort::InsertSort(_array);
-	cout<<endl;
-	cout<<"result: ";
-	CSort::print(_array);
-	cout<<endl;
+	for (int gap = a.size()/2;gap > 0;gap /= 2)//缩小间隔
+	{
+		for (int i = gap;i < a.size();i++)
+		{
+			for (int j = i - gap;j >= 0 && a[j] > a[j+gap];j -= gap)//等间隔比较 插入
+			{
+				swap(a[j],a[j+gap]);
+			}
+		}
+	}
+}
+//选择排序 每一次循环选择最小的放到没有固定位置的最左侧
+void CSort::SelectSort(vector<int> &a)
+{
+	for (int i = 0;i < a.size();i++)
+	{
+		int k = i;
+		for (int j = i+1;j < a.size();j++)
+		{
+			if (a[j] < a[k]) k = j;
+		}
+		swap(a[k],a[i]);
+	}
+}
+
+//归并排序 一个数组分成两个数组 对两个数组分别排序 再合并 
+//分成的两个数组又可以再分（递归）
+
+void CSort::MergeSort(vector<int> &a)
+{
+	mergesort(a,0,a.size()-1);
+}
+//一个数组的左半侧[left,middle]和右半侧[middle+1,right]分别是有序的，合并得到一个有序的数组
+void CSort::MergeArray(vector<int> &a,int left,int middle,int right)
+{
+	if(right == 2)
+	{
+
+	}
+	vector<int> t(right-left+1);
+	int i = left,j = middle+1,k = 0;
+	while(i <= middle && j <= right)//都还要数据时
+	{
+		if (a[i] < a[j]) t[k++] = a[i++];
+		else t[k++] = a[j++];
+	}
+	//只剩一个有数据
+	while(i <= middle) t[k++] = a[i++];
+	while(j <= right) t[k++] = a[j++];
+
+	for (i = left;i <= right;i++) a[i] = t[i-left];
+}
+//归并的递归函数
+void CSort::mergesort(vector<int> &a,int left,int right)
+{
+
+	if (left < right)
+	{
+		
+		int middle = (left + right) / 2;
+		mergesort(a,left,middle);
+		mergesort(a,middle + 1,right);
+		MergeArray(a,left,middle,right);
+	}
+
+}
+//快速排序 任意一个数 定位 在分别排它左侧的 和 右侧的
+void CSort::QuickSort(vector<int> &a)
+{
+	qiucksort(a,0,a.size()-1);
+}
+
+//快排的递归
+void CSort::qiucksort(vector<int> &a,int left,int right)
+{
+	if (left < right)  
+	{  
+	
+		int i = left, j = right, x = a[left];  
+		while (i < j)  
+		{  
+			// 从右向左找第一个小于x的数 
+			while(i < j && a[j] >= x)  j--;	  
+			if(i < j) a[i++] = a[j];
+			// 从左向右找第一个大于等于x的数 
+			while(i < j && a[i] < x) i++;    
+			if(i < j) a[j--] = a[i];  
+		}  
+		a[i] = x;  
+		qiucksort(a, left, i - 1); // 递归调用   
+		qiucksort(a, i + 1, right);  
+	}  
+
 }
